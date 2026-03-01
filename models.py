071005d4d396398f77340316f24a1c305b9ae2ef
Mousssaa <moussanour578@gmail.com>
2026-03-01 06:12:10 +0000

    creating models schemas and database ORM

---
+++ models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeigKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    
+    notes = relationship("Note", back_populates="owner")
+
+class Note(Base):
+    __tablename__ = "notes"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(Text)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    
+    owner = relationship("User", back_populates="notes")
+
