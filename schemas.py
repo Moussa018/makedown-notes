94eecdb4bcd0cb41dc7834a4640d25d9ec2c4011
Mousssaa <moussanour578@gmail.com>
2026-03-01 06:15:31 +0000

    creating models schemas and database ORM

--- schemas.py
+++ schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
+#How the data will be sent to the API and how it will be stored in the database
 class NoteBase(BaseModel):
     title: str
     content: str
@@ -18,3 +19,16 @@ class Note(NoteBase):
     owner_id: int
     class Config:
         orm_mode = True
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    notes: list[Note] = []
+    class Config:
+        from_attributes = True
